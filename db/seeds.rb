f35ed03623582d08cb55043374c45fddae83af5c
Andrew Yermoshin <andrew.yermoshin@gmail.com>
2017-01-24 18:35:04 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,12 @@
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+Feed.create(name: 'Google main news', url: "https://news.google.com/news?&output=rss", category: 'politics')
+Feed.create(name: 'Google main news', url: "https://news.google.com/news?cf=all&hl=en&ned=us&topic=s&output=rss", category: 'sports')
+
+['Trump', 'Rooney', 'football'].each do |word|
+  Keyword.create( content: word, category: 1)
+end
+
+['Brexit', 'Syria', 'bank'].each do |word|
+  Keyword.create( content: word, category: 2)
+end
